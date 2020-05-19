601bc58604735f5767c90de47973cdebb80dcf7c
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-19 13:20:01 -0400

    comparator for repos written

--- repoProcessing.cpp
+++ repoProcessing.cpp
@@ -6,9 +6,29 @@ using namespace std;
 
 // comparator to sort URLs
 // highest percentage of files that are written in python are favoured
+// ties are broken based off of url length, with priority to shorter URLs
 class pyPercentCompare
 {
+    public:
 
+        bool operator ()(const repoURL & repo1, const repoURL & repo2)
+        {
+            double p1 = repo1.getPyPercent();
+            double p2 = repo2.getPyPercent();
+
+            string url1 = repo1.getURL();
+            string url2 = repo2.getURL();
+
+            if (p1 == p2)
+            {
+                if (url1.length() == url2.length())
+                { return true; }
+
+                return url1.length() > url2.length();
+            }
+
+            return p1 > p2;
+        }
 };
 
 // class for repoURLs
@@ -22,8 +42,8 @@ class repoURL
     public:
 
         repoURL(string urlIn, double percentIn);
-        string getURL();
-        double getPyPercent();
+        string getURL() const;
+        double getPyPercent() const;
 };
 
 int main(int argc, char * argv[])
@@ -45,13 +65,13 @@ repoURL::repoURL(string urlIn, double percentIn)
 }
 
 // getter function - returns string url
-string repoURL::getURL()
+string repoURL::getURL() const
 {
     return url;
 }
 
 // getter function - returns pyPercent
-double repoURL::getPyPercent()
+double repoURL::getPyPercent() const
 {
     return pyPercent;
 }
