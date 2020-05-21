9e0653aaa1374a5c106423255c483838b0ed5e05
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-21 16:13:57 -0400

    reads authentication info from file

--- requester.py
+++ requester.py
@@ -1,15 +1,24 @@
 # Github API requester
 import queue
+import requests
 from ratelimit import limits
 
 ONE_MINUTE = 60
 
 # this decoration will not allow this function to make more than 30 API calls per minute
 @limits(calls=30, period=ONE_MINUTE)
-def callGitAPI(repoName):
+def callGitAPI(repoName, auth):
     # prep github API call
     return None
 
+# read file for authentication information
+authFile = open('authInfo.txt', 'r')
+username = authFile.readline().rstrip('\n')
+token = authFile.readline().rstrip('\n')
+authFile.close()
+
+print(username)
+print(token)
 
 repoQueue = queue.Queue()
 
