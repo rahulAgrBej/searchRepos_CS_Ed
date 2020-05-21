228a6e2af76b2b94049bf23c6962c9d7e6aa1c05
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-21 18:49:04 -0400

    finds BeautifulSoup files - working on getting correct url for download of file

--- requester.py
+++ requester.py
@@ -4,28 +4,36 @@ import requests
 from ratelimit import limits, RateLimitException
 from backoff import on_exception, expo
 import time
+import pprint
 
 ONE_MINUTE = 60
 
 def backoff_hdlr(details):
     print("SLEEPS")
-    time.sleep(30)
+    time.sleep(60)
     
 
 # this decoration will not allow this function to make more than 30 API calls per minute
-# if rate is exceeded us an exponential back off method to better time requests
+# if rate is exceeded then will wait 60 seconds to make another request and then use an exponential back off method
 @on_exception(expo, RateLimitException, on_backoff=backoff_hdlr, max_tries=8)
 @limits(calls=30, period=ONE_MINUTE)
-def callGitAPI(repoName, authCreds):
+def callGitAPI(repoName, authCreds):#####, lastTime):
     # prep github API call
-
+    '''
+    currTime = time.perf_counter()
+    diffTime = currTime - lastTime
+    if (diffTime < 2):
+        time.sleep(2-diffTime)
+    '''
     gitAPIQuery = 'https://api.github.com/search/code?q=BeautifulSoup+in:file+language:python+repo:'
     fullQuery = gitAPIQuery + repoName
     response = requests.get(fullQuery, auth=authCreds)
+    #########lastTime = time.perf_counter()
 
     if response.status_code != 200:
-        return {'total_count': 0}
-    return response.json()
+        return {'total_count': 0}####, lastTime
+
+    return response.json()#####, lastTime
 
 # read file for authentication information
 authFile = open('authInfo.txt', 'r')
@@ -52,15 +60,10 @@ repoFile.close()
 
 # testing authentication token
 
+#####lastTime = time.perf_counter()
 for i in range(30):
     currRepo = repoQueue.get()
-    resp = callGitAPI(currRepo, (username, token))
+    resp = callGitAPI(currRepo, (username, token))####, lastTime)
     if resp['total_count'] > 0:
-        print('FOUND ONE!!!!')
-        for it in resp['items']:
-            print(currRepo)
-            print(it['name'])
-    else:
-        print("NOPE")
-
-print("DONE")
+        pp = pprint.PrettyPrinter(indent=3)
+        pp.pprint(resp)
