9f9adaca78b6168cd3df474243491e1962f92ac8
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-27 20:56:07 -0400

    example of fetching a desired file

--- getContent.py
+++ getContent.py
@@ -9,17 +9,22 @@ contentURLFile.close()
 authFile = open('authInfo.txt', 'r')
 username = authFile.readline()
 secret = authFile.readline()
+authFile.close()
 
+
+# gets contents of all desired files and saves them
 for i in range(len(contentURLs)):
      
      currContentURL = contentURLs[i].rstrip('\n')
      print(currContentURL)
 
-     resp = requests.get(currContentURL)
+     resp = requests.get(currContentURL, auth=(username, secret))
+     print(resp.headers)
      resultData = resp.json()
      print(f'response code: {resp.status_code}')
      print(resultData.keys())
      print(resultData['name'])
+     print(f'file size: {resultData["size"]}')
 
      fileName = resultData['name']
      fileBytesEncoded = resultData['content']
@@ -27,7 +32,7 @@ for i in range(len(contentURLs)):
      fileASCII = fileBytesDecoded.decode('ascii')
 
      # open file with fileName and output content into it
-     filePy = open(f'Data/{fileName}', 'w')
+     filePy = open(f'Data/Desired_Files/{fileName}', 'w')
      filePy.write(fileASCII)
      filePy.close()
 
