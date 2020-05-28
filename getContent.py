9102a5adedb26675ae3d5e1638a412dba0d74ce9
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-27 22:35:06 -0400

    changed decoding instead of ascii now does utf-8 since this one includes mandarin characters

--- getContent.py
+++ getContent.py
@@ -2,6 +2,41 @@
 import requests
 import base64
 
+# returns the fileSize of the desired file given the url
+def getFileContents(fileURL, authCreds):
+    
+    resp = requests.get(fileURL, auth=authCreds)
+    
+    print(resp.status_code)
+    
+    while resp.status_code == 403:
+        time.sleep(60)
+        print('retrying previous 403 request')
+        resp = requests.get(fileURL, auth=authCreds)
+
+    # check the request header to see if there are any remaining requests for this window
+    reqsRemaining = int(resp.headers['X-RateLimit-Remaining'])
+    waitTime = 0
+    
+    # check to see when X-RateLimit-Reset happens to resume requests (in UTC epoch seconds)
+    if (reqsRemaining == 0):
+        reqReset = datetime.fromtimestamp(int(resp.headers['X-RateLimit-Reset']))
+        currTime = datetime.now()
+        waitTime = math.ceil((reqReset - currTime).total_seconds())
+    
+    # get file bytes and decode them
+    resultData = resp.json()
+    fileEncoding = resultData['encoding'].rstrip('\n')
+    
+    if (fileEncoding != 'base64'):
+         print(f'file encoding is {fileEncoding}')
+     
+    fileBytesEncoded = resultData['content']
+    fileBytesDecoded = base64.b64decode(fileBytesEncoded)
+    fileASCII = fileBytesDecoded.decode('utf-8')
+
+    return fileASCII
+
 contentURLFile = open('Data/desiredFiles.txt', 'r')
 contentURLs = contentURLFile.readlines()
 contentURLFile.close()
@@ -14,25 +49,15 @@ authFile.close()
 # gets contents of all desired files and saves them
 for i in range(len(contentURLs)):
      
+     # make a request for file contents to the Github API
      currContentURL = contentURLs[i].rstrip('\n')
-     print(currContentURL)
+     fileContents = getFileContents(currContentURL, (username, secret))
 
-     resp = requests.get(currContentURL, auth=(username, secret))
-     print(resp.headers)
-     resultData = resp.json()
-     print(f'response code: {resp.status_code}')
-     print(resultData.keys())
-     print(resultData['name'])
-     print(f'file size: {resultData['size']}')
-
-     fileName = resultData['name']
-     fileBytesEncoded = resultData['content']
-     fileBytesDecoded = base64.b64decode(fileBytesEncoded)
-     fileASCII = fileBytesDecoded.decode('ascii')
+     fileName = 'file' + str(i) + '.py'
 
      # open file with fileName and output content into it
      filePy = open(f'Data/Desired_Files/{fileName}', 'w')
-     filePy.write(fileASCII)
+     filePy.write(fileContents)
      filePy.close()
 
-     break
+print('DONE!')
